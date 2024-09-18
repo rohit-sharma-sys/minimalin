e9762eeeca7e62988c989fad2f507b5fbc8f7367
bonafide-backend <bonafide.backend@gmail.com>
2024-09-18 10:45:20 +0530

    fd

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,25 +1,47 @@
 import React, { useState, useEffect } from "react";
+import LOader from "./Pages/LOader";
+import { Link } from "react-router-dom";
 
 function CatComponent() {
-  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://dummyjson.com/products/category-list')
-    .then(res => res.json())
-    .then(data => {
-        setProducts(data)
-        })
+      setLoading(true); 
+      fetch('https://dummyjson.com/products/categories') 
+          .then(res => res.json())
+          .then(data => {
+              setCategories(data); 
+              setLoading(false); 
+          })
+          .catch(error => {
+              console.error('Error fetching categories:', error);
+              setLoading(false); 
+          });
   }, []);
 
+  if (loading) return <LOader />; 
+
   return (
-    <>   
-      <h1 className="cat-main-section">Categories</h1>
-      <div className="cat-main">
-        {products.slice(4, 8).map((product, index) => (
-            <div className="product-cat" key={index}>{product.replace(/-/g, ' ')}</div>  
-        ))}
+      <div className="categories-container container-with">
+          <h1 class="bft-collection">Categories<span class="dots">.</span></h1>
+          <div className="category-grid">
+              {categories.slice(4,8).map((category, index) => (
+                  <Link to={`/SingleCategory/${category.slug}`} key={index} className="category-card-link">
+                      <div className="category-card">
+                          {/* <img 
+                              src={category.url}  
+                              alt={category.name} 
+                              className="category-image" 
+                          /> */}
+                          <div className="category-details">
+                              <h4 className="category-title">{category.name}</h4> 
+                          </div>
+                      </div>
+                  </Link>
+              ))}
+          </div>
       </div>
-    </>
   );
 }
 
